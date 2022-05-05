545f6268e76ce26a3ae7b1c6ba39210d155a0bdd
SamarNegm <negmsama890@gmail.com>
2022-05-05 12:44:26 +0200

    delete post

--- controllers/posts.js
+++ controllers/posts.js
@@ -1,6 +1,6 @@
 import { catchAsync } from '../utils/utils.js';
 import Post from '../models/Post.js';
-
+import mongoose from 'mongoose';
 export const getPosts = catchAsync(async (req, res) => {
     let query = JSON.stringify(req.query);
     query = query.replace(/(gt|gte|lt|lte)/, (match) => `$${match}`);
@@ -25,4 +25,13 @@ export const createPost = catchAsync(async (req, res, next) => {
     console.log("create Post");
     const posts = await Post.create(req.body);
     res.status(201).json({ status: "success", data: posts });
+});
+export const deletePost = catchAsync(async (req, res, next) => {
+    const { id } = req.params;
+
+    if (!mongoose.Types.ObjectId.isValid(id)) return res.status(404).send(`No post with id: ${id}`);
+
+    await Post.findByIdAndRemove(id);
+
+    res.json({ message: "Post deleted successfully." });
 });
