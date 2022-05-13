49577e9557f6e3299d78f1bbf3f168ff6af7f669
SamarNegm <negmsama890@gmail.com>
2022-05-13 23:28:04 +0200

    sign in

--- controllers/users.js
+++ controllers/users.js
@@ -18,7 +18,17 @@ export const signup = catchAsync(async (req, res) => {
     const token = jwt.sign({ email: user.email, id: user._id }, JWT_SECRET, { expiresIn: "1h" });
 
     res.status(202).json({ message: "Success", data: user, token });
+});
+export const signin = catchAsync(async (req, res) => {
+    const { password, email } = req.body;
+    const existUser = await User.findOne({ email });
+    const isPasswordCorrect = await bcrypt.compare(password, existUser.password);
+    if (!existUser || !isPasswordCorrect) {
+        console.log(existUser, isPasswordCorrect);
+        return res.status(400).json({ message: "Success", data: "User Email or password are not valid" });
 
+    }
+    const token = jwt.sign({ email: existUser.email, id: existUser._id }, JWT_SECRET, { expiresIn: "1h" });
 
-
+    res.status(202).json({ message: "Success", data: existUser, token });
 });
--- routes/users.js
+++ routes/users.js
@@ -1,7 +1,9 @@
 import express from 'express';
-import { signup } from './../controllers/users.js'
+import { signup, signin } from './../controllers/users.js'
 const router = express.Router();
 
 router.get('/signup', signup);
+router.get('/signin', signin);
+
 
 export default router;
